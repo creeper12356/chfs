0ad8583c6a80b184b6efacb08b750002e86d5452
creeper <creeperhjt@sjtu.edu.cn>
2024-11-27 10:07:48 +0000

    add checkpoint
    finish lab2

--- src/distributed/commit_log.cc
+++ src/distributed/commit_log.cc
@@ -22,9 +22,7 @@ CommitLog::~CommitLog() {}
 
 // {Your code here}
 auto CommitLog::get_log_entry_num() -> usize {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
-  return 0;
+  return log_entry_num_;
 }
 
 // {Your code here}
@@ -61,16 +59,30 @@ auto CommitLog::append_log(txn_id_t txn_id,
 
 // {Your code here}
 auto CommitLog::commit_log(txn_id_t txn_id) -> void {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  // 使用一条空的日志来标记事务的提交
+  // NOTE: 可读性很差
+  // <txn_id, 0, empty_block_state> 
+  auto commit_ops = std::vector<std::shared_ptr<BlockOperation>>();
+  commit_ops.push_back(std::make_shared<BlockOperation>(0, std::vector<u8>(bm_->block_size(), 0)));
+  this->append_log(txn_id, commit_ops);
 }
 
 
 auto CommitLog::read_log_ops() const -> std::vector<std::shared_ptr<BlockOperation>> {
+  auto entries = read_log_entries([](txn_id_t txn_id) { return true; });
+  auto ops = std::vector<std::shared_ptr<BlockOperation>>();
+  for(auto &entry: entries) {
+    ops.push_back(std::make_shared<BlockOperation>(entry->block_id, entry->block_state));
+  }
+  return ops;
+}
+
+auto CommitLog::read_log_entries(std::function<bool(txn_id_t)> filter) const 
+  -> std::vector<std::shared_ptr<RedoLogEntry>> {
   auto log_head_ptr = bm_->get_log_head_ptr();
   auto log_content_len = *reinterpret_cast<u32 *>(log_head_ptr);
 
-  auto ops = std::vector<std::shared_ptr<BlockOperation>>();
+  auto log_entries = std::vector<std::shared_ptr<RedoLogEntry>>();
 
   // 单个日志结构
   // <u32 txn_id, u64 block_id, std::vector<u8> block_state>
@@ -86,17 +98,29 @@ auto CommitLog::read_log_ops() const -> std::vector<std::shared_ptr<BlockOperati
     auto block_state = std::vector<u8>(bm_->block_size());
     memcpy(block_state.data(), log_content_ptr, bm_->block_size());
     log_content_ptr += bm_->block_size();
-
-    ops.push_back(std::make_shared<BlockOperation>(block_id, block_state));
+    if(filter(txn_id)) {
+      log_entries.push_back(
+        std::make_shared<RedoLogEntry>(txn_id, block_id, block_state)
+      );
+    }
   }
 
-  return ops;
+  return log_entries;
 }
+
+
 
 // {Your code here}
 auto CommitLog::checkpoint() -> void {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  auto entries = read_log_entries([](txn_id_t txn_id) { return true; });
+  auto commited_txn_ids = std::vector<txn_id_t>();
+  for(auto &entry: entries) {
+    if(entry->block_id == 0) {
+      commited_txn_ids.push_back(entry->txn_id);
+    }
+  }
+
+  
 }
 
 // {Your code here}
@@ -111,6 +135,11 @@ auto CommitLog::recover_with_ret() -> bool {
 
   // 重放日志
   for(auto &op: ops) {
+    if(op->block_id_ == 0) {
+      // 提交日志，不需要处理
+      continue;
+    }
+
     auto res = bm_->write_block(op->block_id_, op->new_block_state_.data());
     if(res.is_err()) {
       return false;
